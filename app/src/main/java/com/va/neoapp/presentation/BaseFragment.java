d56c0e003988b8663ffe35e488bc499376294aa3
varuntej411 <derangulavaruntej@gmail.com>
2021-05-03 12:45:19 +0530

    BaseFragment created

--- app/src/main/java/com/va/neoapp/presentation/BaseFragment.java
+++ app/src/main/java/com/va/neoapp/presentation/BaseFragment.java
@@ -1,25 +1,37 @@
 package com.va.neoapp.presentation;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public abstract class BaseFragment extends Fragment {
 
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = setLayoutResource(inflater, container, savedInstanceState);
+        initGUI(savedInstanceState);
+        initData();
+        return view;
+
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initGUI(savedInstanceState);
-        initData();
     }
+
+    protected abstract View setLayoutResource(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
 
     protected abstract void initGUI(Bundle savedInstanceState);
 
-
     protected abstract void initData();
-
-
 
 
    /* public interface Callback {
