bcbee20829581002ef94ecc176de33cccdd40457
Vivek <vickyvivek2727@gmail.com>
2021-05-03 12:34:14 +0530

    added home activity

---
+++ app/src/main/java/com/va/neoapp/presentation/home/activities/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.va.neoapp.presentation.home.activities;
+
+import android.os.Bundle;
+
+import com.va.neoapp.R;
+import com.va.neoapp.presentation.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected int setLayoutResource() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initGUI(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/va/neoapp/presentation/home/fragments/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.va.neoapp.presentation.home.fragments;
+
+import android.os.Bundle;
+
+import com.va.neoapp.presentation.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+
+
+    @Override
+    protected void initGUI(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+}
