7fb3e4a7c3369cefa9590db821d70e9bb96b114e
varuntej411 <derangulavaruntej@gmail.com>
2021-05-03 12:27:23 +0530

    BaseFragment created

--- app/src/main/java/com/va/neoapp/presentation/BaseFragment.java
+++ app/src/main/java/com/va/neoapp/presentation/BaseFragment.java
@@ -1,4 +1,32 @@
 package com.va.neoapp.presentation;
 
-public class BaseFragment {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initGUI(savedInstanceState);
+        initData();
+    }
+
+    protected abstract void initGUI(Bundle savedInstanceState);
+
+
+    protected abstract void initData();
+
+
+
+
+   /* public interface Callback {
+        void onFragmentAttached();
+
+        void onFragmentDetached(String tag);
+    }*/
+
+
 }
