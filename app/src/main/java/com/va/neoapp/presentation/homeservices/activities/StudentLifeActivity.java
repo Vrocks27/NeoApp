264281bed457f84168cd6a484bd81d47f407c433
Vivek <vickyvivek2727@gmail.com>
2021-05-13 13:37:17 +0530

    added home services

---
+++ app/src/main/java/com/va/neoapp/presentation/homeservices/activities/StudentLifeActivity.java
@@ -0,0 +1,23 @@
+package com.va.neoapp.presentation.homeservices.activities;
+
+import android.os.Bundle;
+
+import com.va.neoapp.presentation.BaseActivity;
+
+public class StudentLifeActivity extends BaseActivity {
+
+    @Override
+    protected int setLayoutResource() {
+        return 0;
+    }
+
+    @Override
+    protected void initGUI(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
